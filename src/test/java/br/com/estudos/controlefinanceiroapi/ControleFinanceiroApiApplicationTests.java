6e34a62af0ab94499e9c1f7dd39155127e5997f0
João Marcelo <joaomarcelo588@gmail.com>
2022-07-05 23:03:54 -0300

    Primeiro commit

---
+++ src/test/java/br/com/estudos/controlefinanceiroapi/ControleFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.controlefinanceiroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
