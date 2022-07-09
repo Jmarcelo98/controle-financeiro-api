d8f573b2a5123f095f1be78a0725c7b6f08128fd
João Marcelo <joaomarcelo588@gmail.com>
2022-07-09 16:01:33 -0300

    Criado verificacao se TipoReceita ja existe

--- src/main/java/br/com/estudos/controlefinanceiroapi/repositories/TipoReceitaRepository.java
+++ src/main/java/br/com/estudos/controlefinanceiroapi/repositories/TipoReceitaRepository.java
@@ -3,7 +3,10 @@ package br.com.estudos.controlefinanceiroapi.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.estudos.controlefinanceiroapi.model.entity.TipoReceita;
+import br.com.estudos.controlefinanceiroapi.model.entity.Usuario;
 
 public interface TipoReceitaRepository extends JpaRepository<TipoReceita, Integer> {
+	
+	Boolean existsByDescricaoAndUsuario(String descricao, Usuario usuario);
  
 }
