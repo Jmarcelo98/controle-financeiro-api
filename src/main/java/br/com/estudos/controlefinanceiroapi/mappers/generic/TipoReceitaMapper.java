6126d4737d3ff7b92af1d60ebad2e1dc39325c8f
João Marcelo <joaomarcelo588@gmail.com>
2022-07-13 11:41:56 -0300

    Retirado upperCase e alterado pesquisa com ignoreCase

---
+++ src/main/java/br/com/estudos/controlefinanceiroapi/mappers/generic/TipoReceitaMapper.java
@@ -0,0 +1,15 @@
+package br.com.estudos.controlefinanceiroapi.mappers.generic;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import br.com.estudos.controlefinanceiroapi.mappers.generic.GenericMapper;
+import br.com.estudos.controlefinanceiroapi.model.dtos.TipoReceitaDTO;
+import br.com.estudos.controlefinanceiroapi.model.entity.TipoReceita;
+
+@Mapper
+public interface TipoReceitaMapper extends GenericMapper<TipoReceita, TipoReceitaDTO>{
+
+	TipoReceitaMapper INSTANCE = Mappers.getMapper(TipoReceitaMapper.class);
+	
+}
