3365a07bfe0f836e560bcd0d7abff961d8ab0a2e
Marcos Giovanny <giovannygigi@gmail.com>
2022-07-13 21:16:33 -0300

    Método atualizar e findAllById adicionado nas classes Receita/TipoReceita

--- src/main/java/br/com/estudos/controlefinanceiroapi/repositories/ReceitaRepository.java
+++ src/main/java/br/com/estudos/controlefinanceiroapi/repositories/ReceitaRepository.java
@@ -1,10 +1,15 @@
 package br.com.estudos.controlefinanceiroapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.estudos.controlefinanceiroapi.model.entity.Receita;
+import br.com.estudos.controlefinanceiroapi.model.entity.Usuario;
 
 
 public interface ReceitaRepository extends JpaRepository<Receita, Integer> {
+	
+	List<Receita> findAllByUsuario(Usuario usuario);
 
 }
--- src/main/java/br/com/estudos/controlefinanceiroapi/services/TipoReceitaService.java
+++ src/main/java/br/com/estudos/controlefinanceiroapi/services/TipoReceitaService.java
@@ -1,8 +1,14 @@
 package br.com.estudos.controlefinanceiroapi.services;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import br.com.estudos.controlefinanceiroapi.handlers.NegocioException;
+import br.com.estudos.controlefinanceiroapi.handlers.RecursoNaoEncontradoException;
+import br.com.estudos.controlefinanceiroapi.mappers.generic.TipoReceitaMapper;
+import br.com.estudos.controlefinanceiroapi.model.dtos.TipoReceitaDTO;
 import br.com.estudos.controlefinanceiroapi.model.entity.TipoReceita;
 import br.com.estudos.controlefinanceiroapi.repositories.TipoReceitaRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class TipoReceitaService {
 
 	private final TipoReceitaRepository tipoReceitaRepository;
+	private final UsuarioService usuarioService;
 
 	public void incluir(TipoReceita tipoReceita) {
 
@@ -20,6 +27,20 @@ public class TipoReceitaService {
 		}
 
 		tipoReceitaRepository.save(tipoReceita);
+	}
+	
+	public List<TipoReceitaDTO> buscarTodosPeloIdUsuario(Integer id){
+		return TipoReceitaMapper.INSTANCE
+				.listaEntityToListaDTO(tipoReceitaRepository.findAllByUsuario(usuarioService.buscarPeloId(id)));
+	}
+	
+	public void atualizar (TipoReceitaDTO tipoReceitaDTO) {
+		var tipoReceita = tipoReceitaRepository.findById(tipoReceitaDTO.getId())
+				.orElseThrow(() -> new RecursoNaoEncontradoException("Tipo de Receita não encontrado pelo ID"));
+		
+		BeanUtils.copyProperties(tipoReceitaDTO, tipoReceita);
+		tipoReceitaRepository.save(tipoReceita);
+		
 	}
 
 	// private metodos
