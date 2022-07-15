9827643f7555c438661fd96b0d8202c694b8fd34
João Marcelo <joaomarcelo588@gmail.com>
2022-07-15 09:36:51 -0300

    Adicionado atributo de foto

--- src/main/java/br/com/estudos/controlefinanceiroapi/model/entity/Usuario.java
+++ src/main/java/br/com/estudos/controlefinanceiroapi/model/entity/Usuario.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -31,9 +33,15 @@ public class Usuario implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nomeCompleto;
+
 	private String cpf;
+
 	private LocalDate dtNascimento;
+
+	@Column(nullable = true)
+	@Lob
 	private byte[] foto;
 
 	@JsonIgnore
